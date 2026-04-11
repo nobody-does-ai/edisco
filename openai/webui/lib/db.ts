4ee411cf63b2a0cc49a0de95912ca8f34f1b6b93
nobody <labhub@turing-trust.com>
2026-04-11 13:31:23 -0400

    x

--- openai/webui/lib/db.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from "pg";
-
-const pool = new Pool({
-  user: process.env.PGUSER || "nobody",
-  database: process.env.PGDATABASE || "nobody",
-  host: process.env.PGHOST || "/var/run/postgresql",
-});
-
-export default pool;
